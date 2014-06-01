20c88a6255ec39f1317f52644dc385e4968f5d96
spawncpy <spawncpy@gmail.com>
2014-06-01 16:44:06 +0200

    Update and rename get.c to kernel_slide.c

--- kernel_slide.c
+++ kernel_slide.c
@@ -1,35 +1,5 @@
 /*  These are just offsets, thanks to iH8sn0w and winocm*/
 
-/*
- * Find the Kernel Base
- */
- 
-void FindKernelBase(void) {
-     kern_return_t Ret;
-     tsk_t Kern_task;
-     vm_region_submap_info_data_64_t Info;
-     vm_size_t size;
-     mach_msg_type_number_t Info_count = VM_REGION_SUBMAP_INFO_COUNT_64;
-     unsigned int Depth = 0;
-     vm_address_t addr = 0x81200000;
-
-    Ret = task_for_pid(mach_task_self(), 0, &kernel_task);
-    if (Ret != KERN_SUCCESS)
-        return 0;
-
-    while (1) {
-      Ret = vm_region_recurse_64(Kernel_task, &Addr, &Size, &Depth, (vm_region_info_t) & Info, &Info_count);
-        if (TRet != KERN_SUCCESS)
-            break;
-        if (size > 1024 * 1024 * 1024)
-            return addr;
-        addr += size;
-    }
-
-    return 0;
-}
-
-// From winocm
 
 /*
  * Get Kernel Slide
